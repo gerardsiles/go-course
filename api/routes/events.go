a26971549ac17492a7a8e11660fd2af97e6198b7
Gerard Siles <gerardsiles@gmail.com>
2024-10-09 17:19:49 +0000

    api

---
+++ api/routes/events.go
@@ -0,0 +1,121 @@
+package routes
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"example.com/rest-api/models"
+	"github.com/gin-gonic/gin"
+)
+
+func getEvent(context *gin.Context) {
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Error converting id"})
+		return
+	}
+	event, err := models.GetEventById(eventId)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "could not get event"})
+		return
+	}
+	context.JSON(http.StatusOK, event)
+}
+
+func getEvents(context *gin.Context) {
+	events, err := models.GetAllEvents()
+	if err != nil {
+		fmt.Println(err)
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Error fetching events"})
+		return
+	}
+
+	context.JSON(http.StatusOK, events)
+}
+func createEvent(context *gin.Context) {
+
+	var event models.Event
+	err := context.ShouldBindJSON(&event)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data"})
+	}
+
+	userId := context.GetInt64("userId")
+	event.UserID = userId
+
+	err = event.Save()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Error creating event"})
+		return
+	}
+
+	context.JSON(http.StatusCreated, gin.H{"message": "event created", "event": event})
+
+}
+
+func updateEvent(context *gin.Context) {
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Error converting id"})
+		return
+	}
+
+	userId := context.GetInt64("userId")
+	event, err := models.GetEventById(eventId)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "could not get event"})
+		return
+	}
+
+	if event.UserID != userId {
+		context.JSON(http.StatusUnauthorized, gin.H{"message": "Not authorized to update event"})
+		return
+	}
+
+	var updatedEvent models.Event
+	err = context.ShouldBindJSON(&updatedEvent)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Invalid data"})
+		return
+	}
+
+	updatedEvent.ID = eventId
+	err = updatedEvent.Update()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "could not update event"})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"message": "Event updated"})
+}
+
+func deleteEvent(context *gin.Context) {
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Error converting id"})
+		return
+	}
+
+	userId := context.GetInt64("userId")
+	event, err := models.GetEventById(eventId)
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "could not get event"})
+		return
+	}
+
+	if event.UserID != userId {
+		context.JSON(http.StatusUnauthorized, gin.H{"message": "Not authorized to delete event"})
+		return
+	}
+
+	err = event.Delete()
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "could not delete event"})
+		return
+	}
+	context.JSON(http.StatusOK, gin.H{"message": "Event deleted successfully"})
+}
--- investment_caulculator.go
+++ investment_caulculator.go
@@ -5,41 +5,40 @@ import (
 	"math"
 )
 
-const inflationRate = 2.5
+const InflationRate = 2.5
+const ReturnRate = 5.5
 
 func main() {
 	var investmentAmount float64 = 1000
-	expectedReturnRate := 5.5
 	var years float64 = 10
 
-	outputText("Investment Amount: ")
-	// & is a pointer to the variable
+	printPrompt("Investment Amount: ")
 	fmt.Scan(&investmentAmount)
 
-	outputText("Expected return rate: ")
-	fmt.Scan(&expectedReturnRate)
+	printPrompt("Expected return rate: ")
+	fmt.Scan(&returnRate)
 
 	fmt.Print("Investment Years: ")
 	fmt.Scan(&years)
 
-	futureValue, futureRealValue := calculateFutureValues(investmentAmount,expectedReturnRate, years)
+	futureValue, futureRealValue := calculateFutureValues(investmentAmount, returnRate, years)
 	// futureValue := (investmentAmount) * math.Pow(1 + expectedReturnRate / 100, (years))
 	// futureRealValue := futureValue / math.Pow(1 + inflationRate / 100, years)
 
 	formattedFV := fmt.Sprintf("Future Value: %.2f\n", futureValue)
 	formattedFRV := fmt.Sprintf("Future value (adjusted for inflation): %.2f\n", futureRealValue)
-	
+
 	// fmt.Printf("Future Value: %.2f\nFuture value (adjusted for inflation): %.2f\n", futureValue, futureRealValue)
 	// fmt.Println("Future value (adjusted for inflation): ",futureRealValue)
 	fmt.Print(formattedFV, formattedFRV)
 }
 
-func outputText(text string) {
+func printPrompt(text string) {
 	fmt.Print(text)
 }
 
-func calculateFutureValues(investmentAmount, expectedReturnRate, years float64) (float64, float64) {
-	fv := investmentAmount * math.Pow(1 + expectedReturnRate / 100, (years))
-	rfv := fv / math.Pow(1 + inflationRate / 100, years)
+func calculateFutureValues(investmentAmount, returnRate, years float64) (float64, float64) {
+	fv := investmentAmount * math.Pow(1+returnRate/100, (years))
+	rfv := fv / math.Pow(1+InflationRate/100, years)
 	return fv, rfv
 }
